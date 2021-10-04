bac9a9186ce62194810f7bb8ef2a9ef0cf605a78
root <root@DESKTOP-NAUB8AU.localdomain>
2021-10-04 00:15:01 -0300

    20211004 00:14 trabalhando com tsyringe

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 import { ImportCategoryController } from "./ImportCategoryController";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
