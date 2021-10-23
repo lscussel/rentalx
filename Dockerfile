6adb675981ad29523255496b285e64a3f43565e9
root <root@DESKTOP-NAUB8AU.localdomain>
2021-10-22 22:47:32 -0300

    202110222245 begginig with session

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node
-WORKDIR /usr/app
+WORKDIR /src/app
 COPY package.json ./
 RUN npm install
 COPY . .
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 
@@ -9,12 +10,19 @@ class CreateUserUseCase {
         private usersRepository: IUsersRepository
     ) { };
 
-    async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void> {
-        this.usersRepository.create({
+    async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+        
+        if (userAlreadyExists) {
+            throw new Error("Email already exists!");
+        }
+
+        const passwordHash = await hash(password, 8);
+        
+        await this.usersRepository.create({
             name,
-            username,
             email,
-            password,
+            password: passwordHash,
             driver_license
         });
     };
