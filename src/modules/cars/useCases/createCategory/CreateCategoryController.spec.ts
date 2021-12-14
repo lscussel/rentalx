59cd0625d69c29b464fce39a8253c6fc87ec559e
lscussel <root@DESKTOP-NAUB8AU.localdomain>
2021-12-13 21:42:10 -0300

    20211213 2140 moving forward supertests

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,13 +1,48 @@
+import { hash } from "bcrypt";
 import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
 
 import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
 
-describe("Create Category Controller", async () => {
+let connection: Connection;
+
+describe("Create Category Controller",() => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuidV4();
+        const password = await hash("admin", 8)
+
+        await connection.query(
+            `INSERT INTO users (id, name, email, password, "isAdmin", driver_license, created_at) 
+            VALUES ('${id}', 'admin', 'admin@analisesparanegocios.com.br', '${password}', true, '888888', 'now()')`
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
     it("Should be able to create a new category", async () => {
+        const responseToken = await request(app).post("/sessions")
+        .send({
+            email: "admin@analisesparanegocios.com.br",
+            password: "admin"
+        });
+
+        const { token } = responseToken.body;
+
         const response = await request(app).post("/categories")
         .send({
             name: "Category Supertest",
             description: "Category Supertest description"
+        })
+        .set({
+            Authorization: `Bearer ${token}`
         });
 
         expect(response.status).toBe(201);
