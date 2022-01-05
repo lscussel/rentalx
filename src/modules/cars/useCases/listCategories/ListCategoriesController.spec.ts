804f9a093a6d1ef598341cbd52a3c259d790d09e
lscussel <root@DESKTOP-NAUB8AU.localdomain>
2022-01-05 20:32:17 -0300

    20220105 2032 refactoring some tests

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -34,7 +34,7 @@ describe("List Category Controller",() => {
             password: "admin"
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token } = responseToken.body;
 
         await request(app).post("/categories")
         .send({
@@ -42,7 +42,7 @@ describe("List Category Controller",() => {
             description: "Category Supertest description"
         })
         .set({
-            Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${refresh_token}`
         });
 
         const response = await request(app).get("/categories");
